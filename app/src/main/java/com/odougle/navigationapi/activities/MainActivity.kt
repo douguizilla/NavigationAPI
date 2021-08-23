89911180fbe859786ed0f64ab55fa046b54a1806
douguizilla <douglasgommez@gmail.com>
2021-08-23 18:00:33 -0300

    set navController on MainActivity.kt to show up actual fragment name on ActionBar

--- app/src/main/java/com/odougle/navigationapi/activities/MainActivity.kt
+++ app/src/main/java/com/odougle/navigationapi/activities/MainActivity.kt
@@ -2,11 +2,29 @@ package com.odougle.navigationapi.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.odougle.navigationapi.R
+import com.odougle.navigationapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        Navigation.findNavController(this, R.id.navHostFragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
