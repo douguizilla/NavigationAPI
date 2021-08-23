260d7fcca35d63b11565737a555cce286c74cfc7
douguizilla <douglasgommez@gmail.com>
2021-08-23 17:55:11 -0300

    implemented onViewCreated calling findNavController to navigate to other fragment by action ID

--- app/src/main/java/com/odougle/navigationapi/fragments/HomeFragment.kt
+++ app/src/main/java/com/odougle/navigationapi/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.odougle.navigationapi.R
 import com.odougle.navigationapi.databinding.FragmentHomeBinding
 
@@ -19,6 +20,13 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnGo.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_completeFragment)
+        }
     }
 
 }
