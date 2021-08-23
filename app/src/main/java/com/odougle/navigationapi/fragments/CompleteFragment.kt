50c5fa4c80dc3e86612766060a99914c46513cb2
douguizilla <douglasgommez@gmail.com>
2021-08-23 17:33:20 -0300

    created CompleteFragment.kt and its layout, putting a string

---
+++ app/src/main/java/com/odougle/navigationapi/fragments/CompleteFragment.kt
@@ -0,0 +1,24 @@
+package com.odougle.navigationapi.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.odougle.navigationapi.R
+import com.odougle.navigationapi.databinding.FragmentCompleteBinding
+
+class CompleteFragment : Fragment() {
+
+    private var _binding: FragmentCompleteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCompleteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
